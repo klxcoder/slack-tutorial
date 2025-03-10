2c2ebd9d782050ae13e0278871cf1e35c0683c18
klxcoder <klxcoder@gmail.com>
2025-03-10 18:30:18 +0700

    Button variant destructive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button>Click me</Button>
+    <Button variant={"destructive"}>Click me</Button>
   );
 }
