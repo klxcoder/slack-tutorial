b252988c275c5fbdc90203e84c1725133d64640b
klxcoder <klxcoder@gmail.com>
2025-03-10 18:33:10 +0700

    Button variant slack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button variant={"destructive"}>Click me</Button>
+    <Button variant={"slack"}>Click me</Button>
   );
 }
