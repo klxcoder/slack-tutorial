f0f487fe07668028aa62b8187c59dc43f0ae9b8e
klxcoder <klxcoder@gmail.com>
2025-03-10 18:34:06 +0700

    Button size lg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button variant={"slack"}>Click me</Button>
+    <Button variant={"slack"} size={"lg"}>Click me</Button>
   );
 }
