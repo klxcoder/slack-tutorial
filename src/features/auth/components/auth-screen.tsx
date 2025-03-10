e2220711ed9633c19f73b766114dadbe136f4bf2
klxcoder <klxcoder@gmail.com>
2025-03-10 18:51:31 +0700

    conditional render SignInCard or SignUpCard

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import { SignInFlow } from "../types";
+import { SignInCard } from "./sign-in-card";
+import { SignUpCard } from "./sign-up-card";
 
 export const AuthScreen = () => {
   const [state, setState] = useState<SignInFlow>("signIn")
@@ -9,7 +11,7 @@ export const AuthScreen = () => {
   return (
     <div className="h-full flex items-center justify-center bg-[#5C3B58]">
       <div className="md:h-auto md:w-[420px]">
-        Auth screen
+        {state === "signIn" ? <SignInCard /> : <SignUpCard />}
       </div>
     </div>
   )
