406e3c99286901f17c5d4b92209dd17f50739f04
klxcoder <klxcoder@gmail.com>
2025-03-11 20:57:13 +0700

    WorkspaceIdLayout

---
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -0,0 +1,13 @@
+interface WorkspaceIdLayoutProps {
+  children: React.ReactNode
+}
+
+const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default WorkspaceIdLayout
