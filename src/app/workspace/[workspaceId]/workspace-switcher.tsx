e8639c48c3a6936e2a7c00462ef07d4f0bacdeb6
klxcoder <klxcoder@gmail.com>
2025-03-12 06:50:47 +0700

    Button switcher

--- src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 export const WorkspaceSwitcher = () => {
   return (
-    <div>
-      Workspace switcher
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button className="size-9 relative overflow-hidden bg-[#ABABAB] hover:bg-[#ABABAB]/80 text-slate-800 font-semibold text-xl">
+          A
+        </Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
   )
 }
