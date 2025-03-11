690189e939bcfe55c1ce7a88545f09b0fcec9b0e
klxcoder <klxcoder@gmail.com>
2025-03-11 09:18:49 +0700

    user can sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { useAuthActions } from "@convex-dev/auth/react";
+
 export default function Home() {
+
+  const { signOut } = useAuthActions();
+
   return (
     <div>
       Logged in!
+      <Button
+        onClick={() => void signOut()}
+      >
+        Sign out
+      </Button>
     </div>
   );
 }
