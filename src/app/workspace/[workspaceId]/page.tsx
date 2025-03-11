bc963ad019ee6f31f4b8b110974d7c64ca459f76
klxcoder <klxcoder@gmail.com>
2025-03-11 20:52:54 +0700

    const { data } = useGetWorkspace({ id: workspaceId })

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,13 +1,15 @@
 "use client"
 
 import { useWorkspaceId } from "@/hooks/use-workspace-id"
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace"
 
 const WorkspaceIdPage = () => {
   const workspaceId = useWorkspaceId()
+  const { data } = useGetWorkspace({ id: workspaceId })
 
   return (
     <div>
-      workspaceId: {workspaceId}
+      Data: {JSON.stringify(data)}
     </div>
   )
 }
