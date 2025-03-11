4482e929efad09ba6d94fcf5feed4b21d2238b95
klxcoder <klxcoder@gmail.com>
2025-03-11 15:18:18 +0700

    workspaces/store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useEffect, useMemo } from "react";
 import { UserButton } from "@/features/auth/components/user-button";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
-import { useCreateWorkspaceModal } from "@/features/store/use-create-workspace-modal";
+import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
 
 export default function Home() {
   const [open, setOpen] = useCreateWorkspaceModal()
