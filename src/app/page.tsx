9ad251b1ea5c1d794fb402c10157e61b3d5e5885
klxcoder <klxcoder@gmail.com>
2025-03-11 17:25:31 +0700

    redirect to first workspace from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,11 @@ import { UserButton } from "@/features/auth/components/user-button";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
+
   const [open, setOpen] = useCreateWorkspaceModal()
 
   const { data, isLoading } = useGetWorkspaces()
@@ -20,12 +23,12 @@ export default function Home() {
     if (isLoading) return;
 
     if (workspaceId) {
-      console.log("Redirect to workspace")
+      router.replace(`/workspace/${workspaceId}`)
     } else if (!open) {
       setOpen(true)
     }
 
-  }, [isLoading, workspaceId, open, setOpen])
+  }, [isLoading, workspaceId, open, setOpen, router])
 
   return (
     <div>
