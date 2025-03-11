24aab7d8202da0add58c7231661bb4e9b7578479
klxcoder <klxcoder@gmail.com>
2025-03-12 06:38:32 +0700

    UserButton to Sidebar

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from "@/features/auth/components/user-button"
+
 export const Sidebar = () => {
   return (
     <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
-      Sidebar
+      <div>A</div>
+      <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
+        <UserButton />
+      </div>
     </aside>
   )
 }
