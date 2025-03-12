986aadc26fa47fda6bed4d31281798545ae59e74
klxcoder <klxcoder@gmail.com>
2025-03-12 18:31:42 +0700

    PreferencesModal

--- src/app/workspace/[workspaceId]/preferences-modal.tsx
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -1,7 +1,40 @@
-export const PreferencesModal = () => {
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogHeader,
+  DialogTitle,
+  DialogClose,
+  DialogFooter,
+} from "@/components/ui/dialog"
+import { useState } from "react"
+
+interface PreferencesModalProps {
+  open: boolean
+  setOpen: (open: boolean) => void
+  initalValue: string,
+}
+
+export const PreferencesModal = ({
+  open,
+  setOpen,
+  initalValue,
+}: PreferencesModalProps) => {
+
+  const [value, setValue] = useState(initalValue)
+
   return (
-    <div>
-      Preferences Modal
-    </div>
+    <Dialog
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            {value}
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   )
 }
--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -10,6 +10,7 @@ import { Doc } from "../../../../convex/_generated/dataModel"
 import { ChevronDown, ListFilter, SquarePen } from "lucide-react"
 import { Hint } from "@/components/hint"
 import { PreferencesModal } from "./preferences-modal"
+import { useState } from "react"
 
 interface WorkspaceHeaderProps {
   workspace: Doc<"workspaces">
@@ -17,9 +18,15 @@ interface WorkspaceHeaderProps {
 }
 
 export const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) => {
+  const [open, setOpen] = useState(false)
+
   return (
     <>
-      <PreferencesModal />
+      <PreferencesModal
+        open={open}
+        setOpen={setOpen}
+        initalValue={workspace.name}
+      />
       <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
@@ -60,7 +67,7 @@ export const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) =>
               <DropdownMenuSeparator />
               <DropdownMenuItem
                 className="cursor-pointer py-2"
-                onClick={() => { }}
+                onClick={() => setOpen(true)}
               >
                 Preferences
               </DropdownMenuItem>
