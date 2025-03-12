b88f3ef06f645c5a2b28606c42bfaf34e99bb156
klxcoder <klxcoder@gmail.com>
2025-03-12 20:56:03 +0700

    schema for table channels

--- convex/schema.ts
+++ convex/schema.ts
@@ -21,6 +21,11 @@ const schema = defineSchema({
     .index("by_user_id", ["userId"])
     .index("wy_workspace_id", ["workspaceId"])
     .index("by_workspace_id_user_id", ["workspaceId", "userId"]),
+  channels: defineTable({
+    name: v.string(),
+    workspaceId: v.id("workspaces")
+  })
+    .index("by_workspace_id", ["workspaceId"]),
   // Your other tables...
 });
 
