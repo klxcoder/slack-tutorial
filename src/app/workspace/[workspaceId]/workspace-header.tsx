d63124a2c618e63195520b72f655a180b3aa54da
klxcoder <klxcoder@gmail.com>
2025-03-12 15:56:42 +0700

    Active workspace

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -3,7 +3,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuSeparator,
+  // DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@radix-ui/react-dropdown-menu"
 import { Doc } from "../../../../convex/_generated/dataModel"
@@ -27,6 +27,24 @@ export const WorkspaceHeader = ({ workspace }: WorkspaceHeaderProps) => {
             <ChevronDown className="size-4 ml-1 shrink-0" />
           </Button>
         </DropdownMenuTrigger>
+        <DropdownMenuContent
+          side="bottom"
+          align="start"
+          className="w-64"
+        >
+          <DropdownMenuItem
+            className="cursor-pointer capitalize flex bg-white p-2 rounded-sm"
+          >
+            <div
+              className="size-9 relative overflow-hidden bg-[#616061] text-white font-semibold text-xl rounded-md flex items-center justify-center mr-2">
+              {workspace.name.charAt(0).toUpperCase()}
+            </div>
+            <div className="flex flex-col items-start">
+              <p className="font-bold">{workspace.name}</p>
+              <p className="text-xs text-muted-foreground">Active workspace</p>
+            </div>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
       </DropdownMenu>
     </div>
   )
