78816f76192547802eb916081147c3ec77685d3e
klxcoder <klxcoder@gmail.com>
2025-03-12 18:48:50 +0700

    button Delete workspace

--- src/app/workspace/[workspaceId]/preferences-modal.tsx
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -7,6 +7,7 @@ import {
   DialogClose,
   DialogFooter,
 } from "@/components/ui/dialog"
+import { TrashIcon } from "lucide-react"
 import { useState } from "react"
 
 interface PreferencesModalProps {
@@ -48,6 +49,14 @@ export const PreferencesModal = ({
               {value}
             </p>
           </div>
+          <button
+            disabled={false}
+            onClick={() => { }}
+            className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50 text-rose-600"
+          >
+            <TrashIcon className="size-4" />
+            <p className="text-sm font-semibold">Delete workspace</p>
+          </button>
         </div>
       </DialogContent>
     </Dialog>
