a5f93592da25fe19a9f380814988914169630929
klxcoder <klxcoder@gmail.com>
2025-03-12 15:32:03 +0700

    Workspace not found

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,7 +1,7 @@
 import { useCurrentMember } from "@/features/members/api/user-current-member"
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace"
 import { useWorkspaceId } from "@/hooks/use-workspace-id"
-import { Loader } from "lucide-react"
+import { AlertTriangle, Loader } from "lucide-react"
 
 export const WorkspaceSidebar = () => {
 
@@ -14,6 +14,17 @@ export const WorkspaceSidebar = () => {
     return (
       <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
         <Loader className="size-5 animate-spin text-white" />
+      </div>
+    )
+  }
+
+  if (!workspace || !member) {
+    return (
+      <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
+        <AlertTriangle className="size-5 text-white" />
+        <p className="text-white text-sm">
+          Workspace not found
+        </p>
       </div>
     )
   }
