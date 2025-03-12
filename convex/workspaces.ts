3fddda9970271441a36cc77e819f4e116dcbf4d0
klxcoder <klxcoder@gmail.com>
2025-03-12 14:15:01 +0700

    Authorization workspaces.get

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -37,7 +37,29 @@ export const create = mutation({
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect()
+    const userId = await getAuthUserId(ctx)
+
+    if (!userId) {
+      return []
+    }
+
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect()
+
+    const workspaceIds = members.map(member => member.workspaceId)
+
+    const workspaces = []
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId)
+      if (workspace) {
+        workspaces.push(workspace)
+      }
+    }
+
+    return workspaces
   }
 })
 
