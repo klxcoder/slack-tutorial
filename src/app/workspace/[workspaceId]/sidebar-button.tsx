04e2dabce311541a3d80ae5bed271cec404803b1
klxcoder <klxcoder@gmail.com>
2025-03-12 08:47:49 +0700

    css for SidebarButton

--- src/app/workspace/[workspaceId]/sidebar-button.tsx
+++ src/app/workspace/[workspaceId]/sidebar-button.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
 import { IconType } from "react-icons/lib";
 
@@ -11,11 +12,17 @@ interface SidebarButtonProps {
 export const SidebarButton = ({
   // icon,
   // label,
-  // isActive,
+  isActive,
 }: SidebarButtonProps) => {
   return (
     <div className="flex flex-col items-center justify-center gap-y-0.5 cursor-pointer group">
-      <Button>
+      <Button
+        variant={"transparent"}
+        className={cn(
+          "size-9 p-2 group-hover:bg-accent/20",
+          isActive && "bg-accent/20"
+        )}
+      >
       </Button>
     </div>
   )
