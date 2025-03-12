bdfd3bf496c015d0a59a28d4ff53406d649d85ab
klxcoder <klxcoder@gmail.com>
2025-03-12 20:05:19 +0700

    confirm dialog before removing workspace

--- src/app/workspace/[workspaceId]/preferences-modal.tsx
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -3,17 +3,16 @@ import {
   Dialog,
   DialogContent,
   DialogFooter,
-  // DialogTrigger,
+  DialogTrigger,
   DialogHeader,
   DialogTitle,
-  // DialogClose,
-  // DialogFooter,
+  DialogClose,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { useRemoveWorkspace } from "@/features/workspaces/api/use-remove-workspace"
 import { useUpdateWorkspace } from "@/features/workspaces/api/use-update-workspace"
+import { useConfirm } from "@/hooks/use-confirm"
 import { useWorkspaceId } from "@/hooks/use-workspace-id"
-import { DialogClose, DialogTrigger } from "@radix-ui/react-dialog"
 import { TrashIcon } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { FormEvent, useState } from "react"
@@ -30,8 +29,12 @@ export const PreferencesModal = ({
   setOpen,
   initalValue,
 }: PreferencesModalProps) => {
-  const workspaceId = useWorkspaceId()
   const router = useRouter()
+  const workspaceId = useWorkspaceId()
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "This action is irreversible",
+  )
 
   const [value, setValue] = useState(initalValue)
   const [editOpen, setEditOpen] = useState(false)
@@ -39,7 +42,11 @@ export const PreferencesModal = ({
   const { mutate: updateWorkspace, isPending: isUpdatingWorkspace } = useUpdateWorkspace()
   const { mutate: removeWorkspace, isPending: isRemovingWorkspace } = useRemoveWorkspace()
 
-  const handleRemove = () => {
+  const handleRemove = async () => {
+    const ok = await confirm()
+
+    if (!ok) return
+
     removeWorkspace({
       id: workspaceId,
     }, {
@@ -70,75 +77,78 @@ export const PreferencesModal = ({
   }
 
   return (
-    <Dialog
-      open={open}
-      onOpenChange={setOpen}
-    >
-      <DialogContent className="p-0 bg-gray-50 overflow-hidden">
-        <DialogHeader className="p-4 border-b bg-white">
-          <DialogTitle>
-            {value}
-          </DialogTitle>
-        </DialogHeader>
-        <div className="px-4 pb-4 flex flex-col gap-y-2">
-          <Dialog open={editOpen} onOpenChange={setEditOpen}>
-            <DialogTrigger asChild>
-              <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
-                <div className="flex items-center justify-between">
-                  <p className="text-sm font-semibold">
-                    Workspace name
-                  </p>
-                  <p className="text-sm text-[#1264a3] hover:underline font-semibold">
-                    Edit
+    <>
+      <ConfirmDialog />
+      <Dialog
+        open={open}
+        onOpenChange={setOpen}
+      >
+        <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+          <DialogHeader className="p-4 border-b bg-white">
+            <DialogTitle>
+              {value}
+            </DialogTitle>
+          </DialogHeader>
+          <div className="px-4 pb-4 flex flex-col gap-y-2">
+            <Dialog open={editOpen} onOpenChange={setEditOpen}>
+              <DialogTrigger asChild>
+                <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                  <div className="flex items-center justify-between">
+                    <p className="text-sm font-semibold">
+                      Workspace name
+                    </p>
+                    <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                      Edit
+                    </p>
+                  </div>
+                  <p className="text-sm">
+                    {value}
                   </p>
                 </div>
-                <p className="text-sm">
-                  {value}
-                </p>
-              </div>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Rename this workspace</DialogTitle>
-              </DialogHeader>
-              <form
-                className="space-y-4"
-                onSubmit={handleEdit}
-              >
-                <Input
-                  value={value}
-                  disabled={isUpdatingWorkspace}
-                  onChange={(e) => setValue(e.target.value)}
-                  required
-                  autoFocus
-                  minLength={3}
-                  maxLength={80}
-                  placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
-                />
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button
-                      variant={"outline"}
-                      disabled={isUpdatingWorkspace}
-                    >
-                      Cancel
-                    </Button>
-                  </DialogClose>
-                  <Button disabled={isUpdatingWorkspace}>Save</Button>
-                </DialogFooter>
-              </form>
-            </DialogContent>
-          </Dialog>
-          <button
-            disabled={isRemovingWorkspace}
-            onClick={handleRemove}
-            className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50 text-rose-600"
-          >
-            <TrashIcon className="size-4" />
-            <p className="text-sm font-semibold">Delete workspace</p>
-          </button>
-        </div>
-      </DialogContent>
-    </Dialog>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Rename this workspace</DialogTitle>
+                </DialogHeader>
+                <form
+                  className="space-y-4"
+                  onSubmit={handleEdit}
+                >
+                  <Input
+                    value={value}
+                    disabled={isUpdatingWorkspace}
+                    onChange={(e) => setValue(e.target.value)}
+                    required
+                    autoFocus
+                    minLength={3}
+                    maxLength={80}
+                    placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+                  />
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button
+                        variant={"outline"}
+                        disabled={isUpdatingWorkspace}
+                      >
+                        Cancel
+                      </Button>
+                    </DialogClose>
+                    <Button disabled={isUpdatingWorkspace}>Save</Button>
+                  </DialogFooter>
+                </form>
+              </DialogContent>
+            </Dialog>
+            <button
+              disabled={isRemovingWorkspace}
+              onClick={handleRemove}
+              className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50 text-rose-600"
+            >
+              <TrashIcon className="size-4" />
+              <p className="text-sm font-semibold">Delete workspace</p>
+            </button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   )
 }
--- src/hooks/use-confirm.tsx
+++ src/hooks/use-confirm.tsx
@@ -1,19 +1,66 @@
-// import { useState } from "react";
+import { ReactElement, useState } from "react";
 
-// import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 
 import {
-  // Dialog,
-  // DialogContent,
-  // DialogDescription,
-  // DialogFooter,
-  // DialogHeader,
-  // DialogTitle,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
 } from "@/components/ui/dialog";
+import { DialogTitle } from "@radix-ui/react-dialog";
 
 export const useConfirm = (
-  // title: string,
-  // message: string,
-): [string, string] => {
-  return ["", ""]
+  title: string,
+  message: string,
+): [() => ReactElement, () => Promise<unknown>] => {
+  const [promise, setPromise] = useState<{ resolve: (value: boolean) => void } | null>(null)
+  const confirm = () => new Promise((resolve) => {
+    setPromise({ resolve })
+  })
+
+  const handleClose = () => {
+    setPromise(null)
+  }
+
+  const handleCancel = () => {
+    promise?.resolve(false)
+    handleClose()
+  }
+
+  const handleConfirm = () => {
+    promise?.resolve(true)
+    handleClose()
+  }
+
+  const ConfirmDialog = () => (
+    <Dialog open={promise !== null}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            {title}
+          </DialogTitle>
+          <DialogDescription>
+            {message}
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="pt-2">
+          <Button
+            onClick={handleCancel}
+            variant={"outline"}
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={handleConfirm}
+          >
+            Confirm
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+
+  return [ConfirmDialog, confirm]
 }
