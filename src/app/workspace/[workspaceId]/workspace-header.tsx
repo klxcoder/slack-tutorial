dd72aa02d4d85a2857cfbb9bdd63d3301e331c37
klxcoder <klxcoder@gmail.com>
2025-03-12 15:41:47 +0700

    WorkspaceHeader

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -1,7 +1,31 @@
-export const WorkspaceHeader = () => {
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu"
+import { Doc } from "../../../../convex/_generated/dataModel"
+
+interface WorkspaceHeaderProps {
+  workspace: Doc<"workspaces">
+}
+
+export const WorkspaceHeader = ({ workspace }: WorkspaceHeaderProps) => {
   return (
-    <div>
-      WorkspaceHeader
+    <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant={"transparent"}
+            className="font-semibold text-lg w-auto p-1.5 overflow-hidden"
+            size={"sm"}
+          >
+            <span>{workspace.name}</span>
+          </Button>
+        </DropdownMenuTrigger>
+      </DropdownMenu>
     </div>
   )
 }
--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -32,7 +32,7 @@ export const WorkspaceSidebar = () => {
 
   return (
     <div className="flex flex-col bg-[#5E2C5F] h-full">
-      <WorkspaceHeader />
+      <WorkspaceHeader workspace={workspace} />
     </div>
   )
 }
