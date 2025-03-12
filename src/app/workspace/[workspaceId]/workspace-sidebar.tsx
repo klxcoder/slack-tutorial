650ae738b31d1ce99ffac51d2c288f748efc41d5
klxcoder <klxcoder@gmail.com>
2025-03-12 15:26:47 +0700

    WorkspaceSidebar loader

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,4 +1,23 @@
+import { useCurrentMember } from "@/features/members/api/user-current-member"
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace"
+import { useWorkspaceId } from "@/hooks/use-workspace-id"
+import { Loader } from "lucide-react"
+
 export const WorkspaceSidebar = () => {
+
+  const workspaceId = useWorkspaceId()
+
+  const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId })
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId })
+
+  if (workspaceLoading || memberLoading) {
+    return (
+      <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
+        <Loader className="size-5 animate-spin text-white" />
+      </div>
+    )
+  }
+
   return (
     <div>
       Workspace Sidebar
