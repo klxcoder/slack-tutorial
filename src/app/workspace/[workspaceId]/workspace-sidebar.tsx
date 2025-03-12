2d2ff1f4fd1ee3a6aba12e1b9df0ac04d54338d3
klxcoder <klxcoder@gmail.com>
2025-03-12 20:38:40 +0700

    SidebarItem Threads

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,8 +1,13 @@
 import { useCurrentMember } from "@/features/members/api/user-current-member"
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace"
 import { useWorkspaceId } from "@/hooks/use-workspace-id"
-import { AlertTriangle, Loader } from "lucide-react"
+import {
+  AlertTriangle,
+  Loader,
+  MessageSquareText,
+} from "lucide-react"
 import { WorkspaceHeader } from "./workspace-header"
+import { SidebarItem } from "./sidebar-item"
 
 export const WorkspaceSidebar = () => {
 
@@ -36,6 +41,13 @@ export const WorkspaceSidebar = () => {
         workspace={workspace}
         isAdmin={member.role === "admin"}
       />
+      <div className="flex flex-col px-2 mt-3">
+        <SidebarItem
+          label="Threads"
+          icon={MessageSquareText}
+          id="threads"
+        />
+      </div>
     </div>
   )
 }
