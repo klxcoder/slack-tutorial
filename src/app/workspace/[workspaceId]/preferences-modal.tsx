35abff35a2b014144099071322da5b990e36a83d
klxcoder <klxcoder@gmail.com>
2025-03-12 18:42:36 +0700

    PreferencesModal Edit

--- src/app/workspace/[workspaceId]/preferences-modal.tsx
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -28,12 +28,27 @@ export const PreferencesModal = ({
       open={open}
       onOpenChange={setOpen}
     >
-      <DialogContent>
-        <DialogHeader>
+      <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+        <DialogHeader className="p-4 border-b bg-white">
           <DialogTitle>
             {value}
           </DialogTitle>
         </DialogHeader>
+        <div className="px-4 pb-4 flex flex-col gap-y-2">
+          <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+            <div className="flex items-center justify-between">
+              <p className="text-sm font-semibold">
+                Workspace name
+              </p>
+              <p className="text-sm text-[#1264a3] hover:underline font-semibold">
+                Edit
+              </p>
+            </div>
+            <p className="text-sm">
+              {value}
+            </p>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   )
