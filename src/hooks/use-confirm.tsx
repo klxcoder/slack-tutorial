c9265a597b36c1ba14d752df4252c1c76f4fc152
klxcoder <klxcoder@gmail.com>
2025-03-12 19:40:23 +0700

    useConfirm

---
+++ src/hooks/use-confirm.tsx
@@ -0,0 +1,19 @@
+// import { useState } from "react";
+
+// import { Button } from "@/components/ui/button";
+
+import {
+  // Dialog,
+  // DialogContent,
+  // DialogDescription,
+  // DialogFooter,
+  // DialogHeader,
+  // DialogTitle,
+} from "@/components/ui/dialog";
+
+export const useConfirm = (
+  // title: string,
+  // message: string,
+): [string, string] => {
+  return ["", ""]
+}
